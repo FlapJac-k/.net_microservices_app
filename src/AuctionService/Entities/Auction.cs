32be5ef9cd2c93a539bc63b5c0ea1ba0394b7128
FlapJac-k <eslamce122@gmail.com>
2025-04-21 06:41:59 +0200

    Added Database and Entities

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,15 @@
+namespace AuctionService.Entities;
+
+public class Auction
+{
+    public Guid Id { get; set; }
+    public int ReservePrice { get; set; } = 0;
+    public required string Seller { get; set; }
+    public string? Winner { get; set; }
+    public int? SoldAmount { get; set; }
+    public int? CurrentHighBid { get; set; }
+    public required DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public required DateTime AuctionEnd { get; set; }
+    public required Status Status { get; set; } = Status.Live;
+    public required Item Item { get; set; }
+}
