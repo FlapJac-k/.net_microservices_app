a236794f006f0c62be26d67a752abf380f71e299
FlapJac-k <eslamce122@gmail.com>
2025-04-28 22:54:55 +0300

    Consuming fault queues

--- src/SearchService/Consumers/AuctionCreatedConsumer.cs
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -14,6 +14,8 @@ public class AuctionCreatedConsumer(IMapper mapper) : IConsumer<AuctionCreated>
 
         var item = mapper.Map<Item>(context.Message);
 
+        if (item.Model == "Foo") throw new ArgumentException("Cannot sell cars with name of Foo");
+
         await item.SaveAsync();
     }
 }
