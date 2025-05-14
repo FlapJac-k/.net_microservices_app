a5b595836a5dfa91a79b9117a1ff67ce31f49091
FlapJac-k <eslamce122@gmail.com>
2025-05-14 22:45:41 +0300

    Adding the DTOs and Automapper

--- src/BiddingService/Consumers/AuctionCreatedConsumer.cs
+++ src/BiddingService/Consumers/AuctionCreatedConsumer.cs
@@ -9,6 +9,7 @@ public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
 {
     public async Task Consume(ConsumeContext<AuctionCreated> context)
     {
+        Console.WriteLine("---> Consuming Auction created");
         var auction = new Auction()
         {
             ID = context.Message.Id.ToString(),
--- src/BiddingService/Controllers/BidsController.cs
+++ src/BiddingService/Controllers/BidsController.cs
@@ -1,3 +1,5 @@
+using AutoMapper;
+using BiddingService.DTOs;
 using BiddingService.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -7,11 +9,11 @@ namespace BiddingService.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class BidController : Controller
+public class BidsController(IMapper mapper) : Controller
 {
     [Authorize]
     [HttpPost]
-    public async Task<ActionResult<Bid>> PlaceBid(string auctionId, int amount)
+    public async Task<ActionResult<BidDto>> PlaceBid(string auctionId, int amount)
     {
         var auction = await DB.Find<Auction>().OneAsync(auctionId);
 
@@ -59,18 +61,18 @@ public class BidController : Controller
 
         await DB.SaveAsync(bid);
 
-        return Ok(bid);
+        return Ok(mapper.Map<BidDto>(bid));
 
     }
 
     [HttpGet("{auctionId}")]
-    public async Task<ActionResult<List<Bid>>> GetBidsForAuction(string auctionId)
+    public async Task<ActionResult<List<BidDto>>> GetBidsForAuction(string auctionId)
     {
         var bids = await DB.Find<Bid>()
             .Match(a => a.AuctionId == auctionId)
             .Sort(b => b.Descending(a => a.BidTime))
             .ExecuteAsync();
         
-        return bids;
+        return bids.Select(mapper.Map<BidDto>).ToList();
     }
 }
