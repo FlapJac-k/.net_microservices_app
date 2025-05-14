29d78e9571031a568e45d3121c8f9bd67c95d7ee
FlapJac-k <eslamce122@gmail.com>
2025-05-14 21:49:08 +0300

    Adding the auction created consumer

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -1,3 +1,4 @@
+using BiddingService.Consumers;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using MassTransit;
@@ -11,6 +12,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddMassTransit(x =>
 {
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("bids", false));
 
     x.UsingRabbitMq((context, cfg) =>
